70438c7278855d586b6fe8c3e8e6ddef43d57dcf
WebApp07 <aminezouguig@proton.me>
2023-10-18 19:52:57 +0100

    we finished clone youtube enjoy & download the code for ffree

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
+import Loader from "./Loader";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
